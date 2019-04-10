9652dba7704d7bdb04d5cecc1e0199ed324bcbfb
Tiffany Phan <Tiffany.Phan@colorado.edu>
2019-04-10 11:51:18 -0600

    Added comments & updated README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Drive-Extension
+
+Created for the person of switching from person email to Colorado account for Google Drive and Google Keep.
+
+### Run
+Go to chrome extensions and select "Load unpacked" and upload the directory.
+
+created for fun
--- redirect.js
+++ redirect.js
@@ -1,4 +1,7 @@
+// Redirects person google account to Colorado account for Google Drive
 if (window.location.href.includes('drive.google.com/drive/u/0/'))
   window.location.href = 'https://drive.google.com/drive/u/1/my-drive'
+
+// Redirects person google account to Colorado account for Google Keep
 if (window.location.href.includes('https://keep.google.com/u/0/'))
   window.location.href = 'https://keep.google.com/u/1/'
