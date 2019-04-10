e898f924354e0514d3c44b3169a654ba54737ebb
Tiffany Phan <Tiffany.Phan@colorado.edu>
2019-04-10 11:32:03 -0600

    extension packages

---
+++ redirect.js
@@ -0,0 +1,4 @@
+if (window.location.href.includes('drive.google.com/drive/u/0/'))
+  window.location.href = 'https://drive.google.com/drive/u/1/my-drive'
+if (window.location.href.includes('https://keep.google.com/u/0/'))
+  window.location.href = 'https://keep.google.com/u/1/'
