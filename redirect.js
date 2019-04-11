b099af2f9a35b0cb9fa777f5a471a7f300da3a49
Tiffany Phan <Tiffany.Phan@colorado.edu>
2019-04-11 14:45:52 -0600

    Fixed infinite redirect

--- redirect.js
+++ redirect.js
@@ -6,8 +6,14 @@ if (window.location.href.includes('drive.google.com/drive/u/0/'))
 if (window.location.href.includes('https://keep.google.com/u/0/'))
   window.location.href = 'https://keep.google.com/u/1/'
 
-if (window.location.href.includes('https://www.google.com/'))
+// Google
+if (window.location.href === 'https://www.google.com/')
   window.location.href = 'https://www.google.com/webhp?authuser=1'
 
-if (window.location.href.includes('https://calendar.google.com/calendar/'))
+// Google Calender
+if (window.location.href === 'https://calendar.google.com/calendar/r')
   window.location.href = 'https://calendar.google.com/calendar/b/1/r?tab=wc&pli=1&t=AKUaPma0YJ4lOXffVhPRRaEwh0OmSx-BDtlfYZ66AM_M_-avNs6ElY-ZN7ILBG_P3uLSCUe2WGgWJrzDzEno8lFrQB-Z-Hc-Mw%3D%3D'
+
+// Google Photos
+if (window.location.href === 'https://photos.google.com/')
+  window.location.href = ('https://photos.google.com/u/1/')
